b9fb9451d00778363f2594586e7d8cf7f97fe6ec
Ananya-G-C-Kundur <ananyagckundur2004@gmail.com>
2025-08-03 21:22:27 +0530

    Added Palindrom Code

---
+++ Palindrom.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Palindrom {
+    public boolean isPalindrome(int x) {
+
+        if(x<0) return false;
+
+        int original = x;
+        int rev = 0;
+
+        while (x>0){
+            int a = x % 10;
+            rev = rev * 10 + a;
+            x = x / 10;
+        }
+
+        return original==rev;
+    }
+
+}
